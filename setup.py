75965f2f80062b63059a0ddcc7754c30df227c03
snower <sujian199@gmail.com>
2016-06-18 14:57:21 +0800

    change version to 0.1.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='torthrift',
-    version='0.1.1',
+    version='0.1.2',
     packages=['torthrift','torthrift.server','torthrift.transport'],
     package_data={
         '': ['README.rst'],
