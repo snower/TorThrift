62982b772b6b5d9b63f218bee8475449106cf0db
snower <sujian199@gmail.com>
2017-02-23 14:58:24 +0800

    change version to 0.1.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='torthrift',
-    version='0.1.4',
+    version='0.1.5',
     packages=['torthrift','torthrift.server','torthrift.transport'],
     package_data={
         '': ['README.rst'],
