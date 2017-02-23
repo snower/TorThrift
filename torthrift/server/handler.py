ed5612e0d8f408a5650d7a48dab01d6495fc9467
snower <sujian199@gmail.com>
2017-02-23 16:47:12 +0800

    fix server handler warp

--- torthrift/server/handler.py
+++ torthrift/server/handler.py
@@ -4,6 +4,7 @@
 
 import sys
 import greenlet
+import functools
 from tornado.ioloop import IOLoop
 from tornado.concurrent import is_future, Future
 
@@ -22,14 +23,15 @@ class HandlerWrapper(object):
         def run(child_gr, *args, **kwargs):
             try:
                 result = func(*args, **kwargs)
+                if not is_future(result):
+                    return child_gr.switch(result)
             except:
                 exc_info = sys.exc_info()
                 result = Future()
                 result.set_exc_info(exc_info)
-            if not is_future(result):
-                return child_gr.switch(result)
             return ioloop.add_future(result, child_gr.switch)
 
+        @functools.wraps(func)
         def _(*args, **kwargs):
             child_gr = greenlet.getcurrent()
             main = child_gr.parent
